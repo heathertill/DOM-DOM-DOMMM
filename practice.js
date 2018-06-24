a829b4e0f2b0413d90b26150a4d46c2301a0ca85
heathertill <heather.till@charter.net>
2018-06-23 22:18:36 -0500

    completed

--- practice.js
+++ practice.js
@@ -3,28 +3,22 @@
 
 
 addEventListener('DOMContentLoaded', function () {
-    //create button
-    let btnDiv = document.createElement('div');
+
     let btn = document.createElement('button');
     let btnLabel = document.createTextNode('Add Square');
-    document.body.appendChild(btnDiv);
-    btnDiv.appendChild(btn);
+    document.body.appendChild(btn);
     btn.appendChild(btnLabel);
     let linebreak = document.createElement("br");
-    linebreak.id = 'linebreak';
     document.body.appendChild(linebreak);
 
-
-    //create boxes 
+    let count = 0;
     let colors = ['hotpink', 'red', 'darkorange', 'limegreen', 'gold', 'royalblue', 'indigo', 'mediumpurple'];
 
     function randomColor(colors) {
         return colors[Math.floor(Math.random() * colors.length)];
     };
 
-    let count = 0;
     btn.addEventListener('click', function () {
-
 
         let divBox = document.createElement('div');
         document.body.appendChild(divBox);
@@ -32,41 +26,54 @@ addEventListener('DOMContentLoaded', function () {
         divBox.style.cssFloat = 'left';
         let boxId = document.createElement('p');
         divBox.appendChild(boxId);
+
         divBox.onselectstart = function () {
             return false;
         };
 
-
-        //create count id
-        let a = count++;
+        let a = count++
         divBox.id = a;
         let textId = document.createTextNode('id = ');
         boxId.appendChild(textId);
         let text = document.createTextNode(divBox.id);
         boxId.appendChild(text);
 
-        //random color on click
+
         divBox.addEventListener('click', function () {
             divBox.style.background = randomColor(colors);
-        })
 
-        //remove box on dblclick
+        })
+       
+
         divBox.addEventListener('dblclick', function () {
 
             if (divBox.id % 2 === 0) {
 
                 if (divBox.nextSibling == null) {
                     alert('No square there!!!')
-                } else { divBox.nextSibling.remove() }
-            } else {
+                } else  {divBox.nextSibling.remove()}
+            } else { 
 
-                if (this.previousSibling.id == 'linebreak') {
+                if (divBox.previousSibling == null) {
                     alert('No square there!!!')
-                } else {
-                    this.previousSibling.remove()
-                }
+                } else {divBox.previousSibling.remove()}
+
+
+
             }
+            
+
+           
+
         })
+
+
+
+
+
+
+
+
     })
 
 
@@ -89,5 +96,6 @@ addEventListener('DOMContentLoaded', function () {
 
 
 
-
+    console.log('fail')
 });
+console.log('fail')
